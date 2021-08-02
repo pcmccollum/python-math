9c6c4e2e1e6f0d74870060c194fe0ee34eb4bea8
Phillip McCollum <pcmccollum@gmail.com>
2021-08-02 08:49:46 -0700

    first commit

---
+++ myturtle.py
@@ -0,0 +1,46 @@
+from turtle import *
+shape('turtle')
+speed(5)
+
+
+def turtleSquares(sideLength=5):
+    for j in range(60):
+        for i in range(4):
+            forward(sideLength)
+            right(90)
+        right(5)
+        sideLength += 5
+
+
+def turtleTriangle(sideLength=100):
+    for i in range(3):
+        forward(sideLength)
+        right(120)
+
+
+def turtlePolygon(sides=5, sideLength=100):
+    for i in range(sides):
+        forward(sideLength)
+        right(360/sides)
+
+
+def turtleStar(sideLength=100):
+    for i in range(5):
+        forward(sideLength)
+        right(180 - (180/5))
+
+
+def turtleStars(sideLength=5):
+    for j in range(60):
+        for i in range(5):
+            forward(sideLength)
+            right(180 - (180/5))
+        right(5)
+        sideLength += 5
+
+
+# turtleStar()
+turtleStars()
+# turtleSquares()
+# turtleTriangle()
+# turtlePolygon(10)
